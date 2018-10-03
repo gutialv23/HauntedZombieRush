feed974aea9de2ed376ff02584c09935dfcae7e5
gutialv23 <alvarogarcia.g23@gmail.com>
2018-10-03 11:59:10 +0200

    ZR01 Creating scenary and movement script

---
+++ Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour {
+
+	[SerializeField] private float Speed     =   1.0f;
+	[SerializeField] private float ResetPosX = -40.0f;
+	[SerializeField] private float DistanceX =  60.5f;
+
+	[SerializeField] private GameObject OtherPlatform;
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (transform.position.x <= ResetPosX)
+		{
+			transform.position = new Vector3( (OtherPlatform.transform.position.x + DistanceX) , transform.position.y , transform.position.z);
+		}
+		else
+		{
+			transform.Translate(Vector3.right * (Speed * Time.deltaTime));
+		}
+	}
+}
