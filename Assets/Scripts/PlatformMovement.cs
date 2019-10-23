3c4ebbb5b8b1126cd802591dd14e9e997e40dafb
gutialv23 <alvarogarcia.g23@gmail.com>
2019-10-24 01:31:28 +0200

    ZR02 Rock movement coroutine

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -1,30 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlatformMovement : MonoBehaviour {
+public class PlatformMovement : MonoBehaviour
+{
+    [SerializeField] private float Speed     =   1.0f;
+    [SerializeField] private float ResetPosX = -40.0f;
+    [SerializeField] private float DistanceX =  60.5f;
 
-	[SerializeField] private float Speed     =   1.0f;
-	[SerializeField] private float ResetPosX = -40.0f;
-	[SerializeField] private float DistanceX =  60.5f;
+    [SerializeField] private GameObject OtherPlatform = null;
 
-	[SerializeField] private GameObject OtherPlatform;
+    // Use this for initialization
+    void Start()
+    {
+    }
 
-	// Use this for initialization
-	void Start()
-	{
-	}
-
-	// Update is called once per frame
-	void Update()
-	{
-		if (transform.position.x <= ResetPosX)
-		{
-			transform.position = new Vector3( (OtherPlatform.transform.position.x + DistanceX) , transform.position.y , transform.position.z);
-		}
-		else
-		{
-			transform.Translate(Vector3.right * (Speed * Time.deltaTime));
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if ( transform.position.x <= ResetPosX )
+        {
+            transform.position = new Vector3( ( OtherPlatform.transform.position.x + DistanceX ), transform.position.y, transform.position.z );
+        }
+        else
+        {
+            transform.Translate( Vector3.right * ( Speed * Time.deltaTime ) );
+        }
+    }
 }
---
+++ Assets/Scripts/RockMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockMovement : MonoBehaviour
+{
+    [ SerializeField ] private float    topPosition =  1.0f;
+    [ SerializeField ] private float bottomPosition = -1.0f;
+    [ SerializeField ] private float speed          =  1.0f;
+    [ SerializeField ] private float waitTime       =  0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine( MoveUpDown() );
+    }
+
+    IEnumerator MoveUpDown()
+    {
+        Vector3 direction = Vector3.down; // Start direction.
+
+        while ( true )
+        {
+            transform.localPosition += ( direction * Time.deltaTime * speed );
+
+            bool wait = false;
+
+                 if ( ( transform.localPosition.y < bottomPosition ) && ( direction != Vector3.up   ) ) { direction = Vector3.up;   wait = true; }
+            else if ( ( transform.localPosition.y >    topPosition ) && ( direction != Vector3.down ) ) { direction = Vector3.down; wait = true; }
+
+            yield return ( ( wait ) ? new WaitForSeconds( waitTime ) : null );
+        }
+    }
+}
