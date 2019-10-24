6c32784f312018fb16e1e21abfd9c34614d39148
gutialv23 <alvarogarcia.g23@gmail.com>
2019-10-25 01:09:59 +0200

    ZR04 New Zombie asset with animations

---
+++ Assets/Scripts/ZombieController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( anim != null )
+        {
+            if ( Input.GetButtonDown( "Jump" ) ) // Jump button is set in "Project Settings -> Input".
+            {
+                anim.Play( "Jump" );
+            }
+        }
+    }
+}
