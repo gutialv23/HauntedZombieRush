4f47fbe2d706d74e3c3b3d73c2d61e12dd5d04fa
gutialv23 <alvarogarcia.g23@gmail.com>
2019-10-30 00:40:23 +0100

    ZR07 Adding death events and sounds

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -1,23 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class ZombieController : MonoBehaviour
 {
-    [SerializeField] private float     jumpForce = 20f;
-    [SerializeField] private AudioClip jumpSound = null;
+    [SerializeField] private float     jumpForce  = 20f;
+    [SerializeField] private AudioClip jumpSound  = null;
+    [SerializeField] private AudioClip deathSound = null;
 
     private Animator    animator    = null;
     private Rigidbody   rigidBody   = null;
     private AudioSource audioSource = null;
     private bool        jump        = false;
 
-    // Start is called before the first frame update.
-    void Start()
+    // Awake is called before Start function and even if the component is not enabled.
+    void Awake()
     {
         animator    = GetComponent< Animator    >();
         rigidBody   = GetComponent< Rigidbody   >();
         audioSource = GetComponent< AudioSource >();
+    }
+
+    // Start is called before the first frame update.
+    void Start()
+    {
+        Assert.IsNotNull( animator   , "Animator component not found"    );
+        Assert.IsNotNull( rigidBody  , "RigidBody component not found"   );
+        Assert.IsNotNull( audioSource, "AudioSource component not found" );
+
+        Assert.IsNotNull( jumpSound  , "Jump Sound not found"            );
+        Assert.IsNotNull( deathSound , "Death Sound not found"           );
     }
 
     // Update is called once per frame.
@@ -56,6 +69,26 @@ public class ZombieController : MonoBehaviour
             {
                 rigidBody.velocity = new Vector3( 0, 0, 0 );
                 rigidBody.AddForce( 0, jumpForce, 0, ForceMode.Impulse );
+            }
+        }
+    }
+
+    // FixedUpdate is called N times per second (frame-rate independent).
+    void OnCollisionEnter( Collision collision )
+    {
+        if ( collision.gameObject.tag == "Obstacle" )
+        {
+            if ( rigidBody != null )
+            {
+                rigidBody.velocity = new Vector3( 0, 0, 0 );
+                rigidBody.AddForce( -5, 5, 0, ForceMode.Impulse );
+                rigidBody.detectCollisions = false;
+            }
+
+            if ( ( audioSource != null ) &&
+                 ( deathSound  != null ) )
+            {
+                audioSource.PlayOneShot( deathSound );
             }
         }
     }
