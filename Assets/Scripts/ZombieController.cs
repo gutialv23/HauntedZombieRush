4fc549adfe4ae86acd119321ecfc307b8022ae87
gutialv23 <alvarogarcia.g23@gmail.com>
2019-10-29 01:17:20 +0100

    ZR05 Zombie jumping and colliding with Physics

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -4,23 +4,43 @@ using UnityEngine;
 
 public class ZombieController : MonoBehaviour
 {
-    private Animator anim;
+    [SerializeField] private float jumpForce = 20f;
 
-    // Start is called before the first frame update
+    private Animator  animator  = null;
+    private Rigidbody rigidBody = null;
+    private bool      jump      = false;
+
+    // Start is called before the first frame update.
     void Start()
     {
-        anim = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    // Update is called once per frame.
     void Update()
     {
-        if ( anim != null )
+        if ( ( animator  != null ) &&
+             ( rigidBody != null ) )
         {
             if ( Input.GetButtonDown( "Jump" ) ) // Jump button is set in "Project Settings -> Input".
             {
-                anim.Play( "Jump" );
+                animator.Play( "Jump" );
+                rigidBody.useGravity = true;
+                jump = true;
             }
+        }
+    }
+
+    // FixedUpdate is called N times per second (frame-rate independent).
+    void FixedUpdate()
+    {
+        if ( jump )
+        {
+            jump = false;
+
+            rigidBody.velocity = new Vector3( 0, 0, 0 );
+            rigidBody.AddForce( 0, jumpForce, 0, ForceMode.Impulse );
         }
     }
 }
